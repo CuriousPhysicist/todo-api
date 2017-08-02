ef66f0a0447a178f8954ab075c0f9df2d0f070fc
CuriousPhysicist <andrewhampson6@gmail.com>
2017-08-02 06:55:53 +0100

    Passing test suite, association and validation tests.

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe Todo, :type => :model do
 	# Validation test
 	# ensure columns title and created_by are present before saving
 	it { should validate_presence_of(:title) }
-	it { should validate_presence_or(:created_by) }
+	it { should validate_presence_of(:created_by) }
 
 end
