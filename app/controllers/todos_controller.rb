8474b40cb8fa9761fc2f96643fcc3ae5f4b6f190
CuriousPhysicist <andrewhampson6@gmail.com>
2017-08-16 12:10:38 +0100

    Creates test suite for Items API.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,13 @@
 class TodosController < ApplicationController
 
+	before_action :set_todo, only: [:show, :update, :destroy]
+
 	def index
 		@todos = Todo.all
 		json_response(@todos)
 	end
 
 	def show
-		@todo = Todo.find(params[:id])
 		json_response(@todo)
 	end
 
@@ -16,15 +17,23 @@ class TodosController < ApplicationController
 	end
 
 	def update
+		@todo.update(todo_params)
+		head :no_content
 	end
 
 	def destroy
+		@todo.destroy
+		head :no_content
 	end
 
 	private
 
 	def todo_params
 		params.permit(:title, :created_by)
+	end
+
+	def set_todo
+		@todo = Todo.find(params[:id])
 	end
 
 end
